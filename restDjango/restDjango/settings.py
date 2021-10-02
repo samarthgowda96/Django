149ab96768dcef3464bba6479046457ff2ee7347
Samarth Gowda <samarthgowda@samarths-mbp.myfiosgateway.com>
2021-10-02 11:17:01 -0400

    first-rest-django-practice

--- restDjango/restDjango/settings.py
+++ restDjango/restDjango/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'firstDjango',
         'USER':'postgres',
-        'PASSWORD':'Mobile21!',
+        
         'HOST':'localhost',
         'PORT':'5432'
     }
